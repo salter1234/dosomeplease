ed801860af83b0fcd07ad2259147b07c7a805727
salter1234 <143507366+salter1234@users.noreply.github.com>
2024-07-27 02:31:38 +0800

    Add files via upload

--- dosomeplease/urls.py
+++ dosomeplease/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('mentalhealth.urls')),
     url(r'mdeditor/', include('mdeditor.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
